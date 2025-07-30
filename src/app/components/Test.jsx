d3017418b5fad8c0b155c079f736187aeeac0fd0
Sushant Chitrakar <sushantchitrakar@gmail.com>
2025-07-30 10:43:38 +1000

    test

---
+++ src/app/components/Test.jsx
@@ -0,0 +1,7 @@
+export default function Test() {
+  return (
+    <div class="bg-green-500 text-white text-center p-4">
+      Tailwind is working!
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import Test from "./components/Test";
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      <Test />
+      {/* <Hero /> */}
       {/* <Services />
       <About />
       <Testimonials />
