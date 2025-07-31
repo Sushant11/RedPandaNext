0c6aaf6a440f7d7cad71a3bf41a830966deac7b1
Sushant Chitrakar <sushantchitrakar@gmail.com>
2025-07-31 12:29:50 +1000

    Email

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -1,31 +1,33 @@
-const nodemailer = require("nodemailer");
+import nodemailer from "nodemailer";
 
-export default async function handler(req, res) {
-  console.log("Received request:", req.method); // Log the method (GET, POST, etc.)
-  if (req.method === "POST") {
-    try {
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.GMAIL_USER,
-          pass: process.env.GMAIL_APP_PASSWORD, // Use the app password here
-        },
-      });
+export async function POST(request) {
+  try {
+    const body = await request.json();
 
-      const mailOptions = {
-        from: process.env.GMAIL_USER, // sender address
-        to: "sushantchitrakar@example.com", // recipient address (your specific recipient email)
-        subject: "New Message from Contact Form",
-        text: `Name: ${req.body.name}\nEmail: ${req.body.email}\nPhone: ${req.body.phone}\nMessage: ${req.body.message}`,
-      };
+    const { name, email, phone, message } = body;
 
-      const info = await transporter.sendMail(mailOptions);
-      res.status(200).json({ message: "Email sent successfully", info });
-    } catch (error) {
-      console.error("Error sending email:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_APP_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.GMAIL_USER,
+      to: "sushantchitrakar@gmail.com",
+      subject: "New Message from Contact Form",
+      text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+
+    return Response.json({ message: "Email sent successfully", info });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+    });
   }
 }
