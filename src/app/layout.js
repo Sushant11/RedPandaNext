a045b3f0f1c8d60232d96d1b5bae0c45b7ab34a2
Sushant Chitrakar <sushantchitrakar@gmail.com>
2025-07-31 13:51:47 +1000

    minor

--- src/app/layout.js
+++ src/app/layout.js
@@ -52,9 +52,9 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         <Analytics />
-        <SpeedInsights />
         <Toaster position="bottom-center" />
         <main>{children}</main>
+        <SpeedInsights />
         <Footer />
         <BackToTop />
       </body>
