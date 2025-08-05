67075d391d87733397210a59cf5bd91511953fed
Sushant Chitrakar <sushantchitrakar@gmail.com>
2025-08-05 14:11:30 +1000

    Email Address

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -53,7 +53,7 @@ export async function POST(request) {
 
     const mailOptions = {
       from: process.env.GMAIL_USER,
-      to: "sushantchitrakar@gmail.com",
+      to: "hello@redpandafinance.com.au",
       subject: "New Website Inquiry Received",
       html: htmlBody,
     };
